ba04055e419a8c3eebcbc17db10fe05ca0ba2a71
larsssmoatsss <moats.lars@gmail.com>
2026-07-22 11:28:54 -0400

    refactor: remove sample skeleton tables and update orders endpoints for new schema

--- FInal_Project/api/controllers/orders.py
+++ FInal_Project/api/controllers/orders.py
@@ -6,8 +6,12 @@ from sqlalchemy.exc import SQLAlchemyError
 
 def create(db: Session, request):
     new_item = model.Order(
-        customer_name=request.customer_name,
-        description=request.description
+        customer_id=request.customer_id,
+        promotion_id=request.promotion_id,
+        tracking_number=request.tracking_number,
+        order_type=request.order_type,
+        status=request.status,
+        total_price=request.total_price
     )
 
     try:
@@ -32,7 +36,7 @@ def read_all(db: Session):
 
 def read_one(db: Session, item_id):
     try:
-        item = db.query(model.Order).filter(model.Order.id == item_id).first()
+        item = db.query(model.Order).filter(model.Order.order_id == item_id).first()
         if not item:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
     except SQLAlchemyError as e:
@@ -43,7 +47,7 @@ def read_one(db: Session, item_id):
 
 def update(db: Session, item_id, request):
     try:
-        item = db.query(model.Order).filter(model.Order.id == item_id)
+        item = db.query(model.Order).filter(model.Order.order_id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         update_data = request.dict(exclude_unset=True)
@@ -57,7 +61,7 @@ def update(db: Session, item_id, request):
 
 def delete(db: Session, item_id):
     try:
-        item = db.query(model.Order).filter(model.Order.id == item_id)
+        item = db.query(model.Order).filter(model.Order.order_id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         item.delete(synchronize_session=False)
--- FInal_Project/api/routers/index.py
+++ FInal_Project/api/routers/index.py
@@ -1,6 +1,5 @@
-from . import orders, order_details
+from . import orders
 
 
 def load_routes(app):
     app.include_router(orders.router)
-    app.include_router(order_details.router)
